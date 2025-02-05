6228bb06eb51b5b16611104d5f4a209539e8466e
Eric Dauenhauer <eric@ericyd.com>
2025-02-05 16:46:34 -0600

    add more useful aliases

--- setup.sh
+++ setup.sh
@@ -42,9 +42,15 @@ cat > customization.sh <<-EOF
 # git config --global user.name My Name
 # git config --global user.email email@domain.com
 # git config --global alias.lg "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)' --all"
-# git config --global alias.l "log --abbrev-commit --decorate --date=short --format=format:'%h, %aI - %s%d' -n 10"
 # git config --global alias.ca "commit --amend --no-edit"
+# git config --global alias.fp "fetch --prune"
 # git config --global alias.pfwl "push --force-with-lease"
+# git config --global alias.l "log --abbrev-commit --decorate --date=short --format=format:'%h, %aI - %s%d' -n 10"
+# git config --global alias.ll "log --abbrev-commit --decorate --date=short --format=format:'%H, %aI - %s%d' -n 5"
+# git config --global alias.cm "commit -m"
+# git config --global alias.s "status"
+# git config --global alias.back1 "reset HEAD~1"
+
 EOF
 chmod +x customization.sh
 
